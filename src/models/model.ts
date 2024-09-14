3f859ca3ab2bfc99a436bfe8c4351ebad07ab502
Lucas Soares <lucassoares2007@hotmail.com>
2024-09-14 03:14:19 -0300

    fix: delete models

--- src/models/model.ts
+++
@@ -1,7 +0,0 @@
-import Database from '../db/pg.js';
-
-class Model {
-  protected pool = Database.getPool();
-}
-
-export default Model;
--- src/models/modelWPP.ts
+++
@@ -1,9 +0,0 @@
-import DatabaseWPP from '../db/wpp.js';
-
-import Model from './model.js';
-
-class ModelWPP extends Model {
-  protected poolWPP = DatabaseWPP.getPool();
-}
-
-export default ModelWPP;
